b0144afe4dc8366fab0b684f81e3271b34a627c2
Md Taufeeque Alam <115384749+mdtaufeeque046@users.noreply.github.com>
2023-01-18 10:20:50 +0530

    Add files via upload

---
+++ Admin Dashboard/my_chart.js
@@ -0,0 +1,65 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Facebook', 'Google', 'Youtube','Instagram', 'Amazon'],
+        datasets: [{
+            label: 'Traffic Source',
+            data: [1200, 2500, 500, 720, 2000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+                
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+
+
+
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June',, 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [1200, 1090, 3000, 5429, 1220, 3300, 4800, 4578, 2566, 7545, 4587, 4587, 9000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+       responsive: true,
+    }
+});
